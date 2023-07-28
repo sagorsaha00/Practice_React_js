b79b24947f23e1a8e5ef3f9d22413347f2a79072
Sagor saha <sahasagor650@gmail.com>
2023-07-28 21:47:43 +0600

    dd

--- increment.js
+++ increment.js
@@ -1,6 +1,28 @@
 const domContainer = document.querySelector("#root");
-const myElement = React.createElement("div" ,null,"hello world");
+// const myElement = React.createElement("div" ,null,"hello world");
 // const element = React.createElement("div", null, "hello world")
 
-ReactDOM.render(myElement , domContainer);
+// ReactDOM.render(myElement , domContainer);
+
 // console.log(domContainer);
+
+const Increment = () =>  {
+    const  [counter, setcounter] = React.useState(0);
+    return(
+            <div>
+        <h1 id="display"> {counter}</h1>
+        <div>
+            <button id="button" onClick = {() => setcounter(counter + 1)}>Increment +
+            </button>
+        </div>
+    </div> 
+    )
+
+};
+
+ReactDOM.render(
+    <div className="container">
+    <Increment/>
+    <Increment/>
+    </div>,
+    domContainer);
